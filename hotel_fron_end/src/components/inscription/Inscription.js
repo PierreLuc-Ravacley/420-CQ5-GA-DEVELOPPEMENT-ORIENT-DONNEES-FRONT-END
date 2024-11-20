d0e826d993884220d2c146d27e26891060e9ec03
2361691 <142833371+DFatalR@users.noreply.github.com>
2024-11-20 16:06:18 -0500

    2.1.3 Composante Inscription
    Concevoir et développer les composants visuels de la page d'inscription du front-end.

---
+++ hotel_fron_end/src/components/inscription/Inscription.js
@@ -0,0 +1,103 @@
+import React, { useState } from "react";
+import './inscriptionStyle.css'; // Ajoute du fichier CSS
+
+function Inscription() {
+  const [nom, setNom] = useState("");
+  const [prenom, setPrenom] = useState("");
+  const [adresse, setAdresse] = useState("");
+  const [telephone, setTelephone] = useState("");
+ //  const [email, setEmail] = useState("");
+  const [motDePasse, setMotDePasse] = useState("");
+  const [confirmationMotDePasse, setConfirmationMotDePasse] = useState("");
+  const [acceptTermes, setAcceptTermes] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Formulaire envoyé !");
+    // logique pour envoyer les données à le backend
+  };
+
+  return (
+    <div className="inscription">
+      <form onSubmit={handleSubmit}>
+        <div className="input-field">
+          <div className="icon-container">
+            <i className="fas fa-user"></i>
+          </div>
+          <input 
+            type="text" 
+            placeholder="Nom" 
+            value={nom} 
+            onChange={(event) => setNom(event.target.value)} 
+          />
+        </div>
+        <div className="input-field">
+          <div className="icon-container">
+            <i className="fas fa-user "></i>
+          </div>
+          <input 
+            type="text" 
+            placeholder="Prénom" 
+            value={prenom} 
+            onChange={(event) => setPrenom(event.target.value)} 
+          />
+        </div>
+        <div className="input-field">
+          <div className="icon-container">
+            <i className="fas fa-home"></i>
+          </div>
+          <input 
+            type="text" 
+            placeholder="Adresse" 
+            value={adresse} 
+            onChange={(event) => setAdresse(event.target.value)} 
+          />
+        </div>
+        <div className="input-field">
+          <div className="icon-container">
+            <i className="fas fa-phone"></i>
+          </div>
+          <input 
+            type="tel" 
+            placeholder="Téléphone" 
+            value={telephone} 
+            onChange={(event) => setTelephone(event.target.value)} 
+          />
+        </div>
+        <div className="input-field">
+          <div className="icon-container">
+            <i className="fas fa-lock"></i>
+          </div>
+          <input 
+            type="password" 
+            placeholder="Mot de passe" 
+            value={motDePasse} 
+            onChange={(event) => setMotDePasse(event.target.value)} 
+          />
+        </div>
+        <div className="input-field">
+          <div className="icon-container">
+            <i className="fas fa-lock"></i>
+          </div>
+          <input 
+            type="password" 
+            placeholder="Confirmer le mot de passe" 
+            value={confirmationMotDePasse} 
+            onChange={(event) => setConfirmationMotDePasse(event.target.value)} 
+          />
+        </div>
+        <div className="checkbox-field">
+          <input 
+            type="checkbox" 
+            checked={acceptTermes} 
+            onChange={() => setAcceptTermes(!acceptTermes)} 
+          />
+          <label className="termes"> J'accepte les termes et conditions</label>
+        </div>
+        <button type="submit" disabled={!acceptTermes}>Créer le compte</button>
+      </form>
+    </div>
+  );
+}
+
+export default Inscription;
